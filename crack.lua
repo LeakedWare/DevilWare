34dde13d6c75f508c3b6c18568b25f979916a937
LeakedWare <139758993+LeakedWare@users.noreply.github.com>
2023-10-10 08:33:34 -0700

    Create crack.lua

---
+++ crack.lua
@@ -0,0 +1,19 @@
+
+local STUFF1 = game:HttpGet("https://raw.githubusercontent.com/LeakedWare/DevilWare/main/hwid.lua")
+local STUFF2 = game:HttpGet("https://raw.githubusercontent.com/LeakedWare/DevilWare/main/key.lua")
+
+local old 
+old = hookfunction(game.HttpGet,function(self,url,...)
+    if type(url) == "string" and string.match(url,"hwid")then 
+            print("Returned Own Script")
+            return STUFF1
+    elseif type(url) == "string" and string.match(url,"key")then 
+        print("Returned Own Script")
+        return STUFF2
+        end
+    return old(self,url,...)
+end)
+
+print"Succesfully executed"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LeakedWare/DevilWare/main/main.lua"))()
